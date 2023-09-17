ef7e6e79c567f33fd2cfc8bd2427ad9b54a8fbfe
Matthew Lloyd <matthew.lloyd@firstassistance.com>
2023-09-18 09:43:59 +1200

    intiial commit

---
+++ FluentSpinner/SpinnerComponent.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import { ISpinnerProps, Spinner } from '@fluentui/react';
+
+function SpinnerComponent(props: ISpinnerProps) {
+  return <Spinner {...props} />;
+}
+
+export default SpinnerComponent;
